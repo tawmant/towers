8126239e2837a02014870157da93abc1b0f5e566
Azamat <azamat.17111711@gmail.com>
2021-07-30 12:34:11 +0600

    update

--- js/apartment-select.js
+++ js/apartment-select.js
@@ -1,69 +1,69 @@
 const room_1 = [
   {
     src: '1kv 38.68.png',
-    title: "38.68 m2"
+    title: "38m2"
   },
   {
     src: '1kv 40.53.png',
-    title: "40.53 m2"
+    title: "40m2"
   },
   {
     src: '1kv 41,06.png',
-    title: "41,06 m2"
+    title: "41m2"
   },
   {
     src: '1kv 43,60.png',
-    title: "43,60 m2"
+    title: "43m2"
   },
   {
     src: '1kv 43.56.png',
-    title: "43.56 m2"
+    title: "43m2"
   },
   {
     src: '1kv 44.05.png',
-    title: "44.05 m2"
+    title: "44m2"
   },
   {
     src: '1kv 44.96.png',
-    title: "44.96 m2"
+    title: "44m2"
   }
 ];
 
 const room_2 = [
   {
     src: '2kv 52.28.png',
-    title: "52.28 m2"
+    title: "52m2"
   },
   {
     src: '2kv 56.93.png',
-    title: "56.93 m2"
+    title: "56m2"
   },
   {
     src: '2kv 66.62.png',
-    title: "66.62 m2"
+    title: "66m2"
   },
   {
     src: '2kv 66.79.png',
-    title: "66.79 m2"
+    title: "66m2"
   },
   {
     src: '2kv 67.99.png',
-    title: "67.99 m2"
+    title: "67m2"
   },
   {
     src: '2kv 70.34.png',
-    title: "70.34 m2"
+    title: "70m2"
   },
 ];
 
 const room_3 = [
   {
     src: '3kv 81.93.png',
-    title: "81.93 m2"
+    title: "81m2"
   },
   {
     src: '3kv 99.36.png',
-    title: "99.36 m2"
+    title: "99m2"
   },
 ];
 
