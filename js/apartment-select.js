f4821d8420657c13794f76e674b131557d4ac10d
Azamat <azamat.17111711@gmail.com>
2021-08-11 13:05:17 +0600

    finish

--- js/apartment-select.js
+++ js/apartment-select.js
@@ -1,15 +1,80 @@
+// const room_1 = [
+// 	{
+// 		src: '1kv 41,06.png',
+// 		title: '41m2',
+// 	},
+// 	{
+// 		src: '1kv 43,60.png',
+// 		title: '42m2',
+// 	},
+// 	{
+// 		src: '1kv 44.05.png',
+// 		title: '44m2',
+// 	},
+// 	{
+// 		src: '1kv 44.96.png',
+// 		title: '45m2',
+// 	},
+// ];
+
+// const room_2 = [
+// 	{
+// 		src: '2kv 52.28.png',
+// 		title: '53m2',
+// 	},
+// 	{
+// 		src: '2kv 56.93.png',
+// 		title: '57m2',
+// 	},
+// 	{
+// 		src: '2kv 67.99.png',
+// 		title: '67m2',
+// 	},
+// 	{
+// 		src: '2kv 66.79.png',
+// 		title: '68m2',
+// 	},
+// 	{
+// 		src: '2kv 70.34.png',
+// 		title: '71m2',
+// 	},
+// ];
+
+// const room_3 = [
+// 	{
+// 		src: '3kv 81.93.png',
+// 		title: '82m2',
+// 	},
+// 	{
+// 		src: '3kv 99.36.png',
+// 		title: '100m2',
+// 	},
+// ];
+
 const room_1 = [
 	{
-		src: '1kv 41,06.png',
+		src: '1kv 38.68.png',
+		title: '38m2',
+	},
+	{
+		src: '1kv 40.53.png',
 		title: '41m2',
 	},
 	{
-		src: '1kv 43,60.png',
+		src: '1kv 41,06.png',
 		title: '42m2',
 	},
 	{
-		src: '1kv 44.05.png',
+		src: '1kv 43,60.png',
 		title: '44m2',
+	},
+	{
+		src: '1kv 43.56.png',
+		title: '44m2',
+	},
+	{
+		src: '1kv 44.05.png',
+		title: '45m2',
 	},
 	{
 		src: '1kv 44.96.png',
@@ -27,11 +92,15 @@ const room_2 = [
 		title: '57m2',
 	},
 	{
-		src: '2kv 67.99.png',
+		src: '2kv 66.62.png',
 		title: '67m2',
 	},
 	{
 		src: '2kv 66.79.png',
+		title: '67m2',
+	},
+	{
+		src: '2kv 67.99.png',
 		title: '68m2',
 	},
 	{
@@ -114,12 +183,12 @@ const apartment_select_modal__img = document.getElementById('com_premises__modal
 
 apartmentSeImg.addEventListener('click', () => {
 	const value = apartmentSeImg.getAttribute('src');
-	apartment_select_modal.classList.add('open')
+	apartment_select_modal.classList.add('open');
 	apartment_select_modal__img.setAttribute('src', value);
 	document.body.style.overflow = 'hidden';
 });
 
 apartment_select_modal.addEventListener('click', () => {
-	apartment_select_modal.classList.remove('open')
+	apartment_select_modal.classList.remove('open');
 	document.body.style.overflow = '';
-})
+});
